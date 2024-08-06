aa8814b179308aa269098034cd1f2f39278b21f8
Sam Erickson <samericksn@gmail.com>
2024-08-05 19:03:41 -0700

    fix: incorrect json structure in responses
    All of the fields that were present in the database record were included
    in responses that included serialized json of a Box.
    
    See:
    https://stackoverflow.com/questions/44003152/hide-fields-in-golang-gorm

--- main.go
+++ main.go
@@ -15,10 +15,10 @@ import (
 )
 
 type Box struct {
-	gorm.Model
-	Name     string `json:"name" gorm:"unique"`
-	Location string `json:"location"`
-	Id       uint   `json:"id" gorm:"primaryKey;autoIncrement"`
+	gorm.Model `json:"-"`
+	Name       string `json:"name" gorm:"unique"`
+	Location   string `json:"location"`
+	Id         uint   `json:"id" gorm:"primaryKey;autoIncrement"`
 }
 
 var (
