4b439159f5c66450738e3c3ef713827f38cc4620
Sam Erickson <samericksn@gmail.com>
2024-09-02 16:27:41 -0700

    feat(box): allow duplicate box names

--- models/box.go
+++ models/box.go
@@ -4,7 +4,7 @@ import "gorm.io/gorm"
 
 type Box struct {
 	gorm.Model `json:"-"`
-	Name       string `json:"name" gorm:"unique"`
+	Name       string `json:"name"`
 	Location   string `json:"location"`
 	Id         uint   `json:"id" gorm:"primaryKey;autoIncrement"`
 }
