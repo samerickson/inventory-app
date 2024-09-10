677ec61f79638d7703b9870d46537eda7e97db18
Sam Erickson <samericksn@gmail.com>
2024-09-09 19:47:22 -0700

    feat: add cors

--- backend/main.go
+++ backend/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"fmt"
 	"inventory-app/persistence"
 	"inventory-app/routes"
 
 	"gorm.io/gorm"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
 )
@@ -17,20 +19,34 @@ type Box struct {
 	Id         uint   `json:"id" gorm:"primaryKey;autoIncrement"`
 }
 
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		fmt.Println("HERE +++++++++++++++++++++++++++++++++++++++")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+		// c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+		// c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT")
+
+		if c.Request.Method == "OPTIONS" {
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
+
 func main() {
 	if persistence.ConnectDb() {
-		r := gin.Default()
+		router := gin.Default()
 
-		r.GET("/", func(c *gin.Context) {
-			c.JSON(200, gin.H{
-				"message": "Hello World",
-			})
-		})
+		// Default allows all origins
+		router.Use(cors.Default())
 
-		v1 := r.Group("/v1")
+		v1 := router.Group("/v1", cors.Default())
 		routes.AddBoxes(v1)
 		routes.AddItems(v1)
 
-		r.Run()
+		router.Run()
 	}
 }
