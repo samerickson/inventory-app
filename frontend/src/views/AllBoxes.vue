bc2e830adfce8a5a0d58c0d12acdc20478fc823d
Sam Erickson <samericksn@gmail.com>
2024-09-14 09:30:44 -0700

    feat: remove default styles

--- frontend/src/views/AllBoxes.vue
+++ frontend/src/views/AllBoxes.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import BoxEntry from '@/components/boxEntry.vue';
+import CreateBox from '@/components/createBox.vue';
 import { ref } from 'vue';
 
 const boxes = ref();
@@ -12,4 +13,5 @@ fetch("http://localhost:8080/v1/box/", {cache: "no-cache"}).then(async (response
 
 <template>
   <BoxEntry v-for="box in boxes" :box="box" :key="box.id"/>
+  <CreateBox />
 </template>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,21 +1,6 @@
-<script setup lang="ts">
-import CreateBox from '@/components/createBox.vue';
-</script>
-
 <template>
   <main>
     <h1>This is an about page</h1>
     <router-view />
-    <CreateBox />
   </main>
 </template>
-
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
