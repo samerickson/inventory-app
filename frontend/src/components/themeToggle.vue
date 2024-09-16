a38ee31d5e74f15fc5fcf2579333ec1b36292211
Sam Erickson <samericksn@gmail.com>
2024-09-15 17:03:34 -0700

    feat: fix theme button

--- frontend/src/components/themeToggle.vue
+++ frontend/src/components/themeToggle.vue
@@ -2,7 +2,6 @@
 import { useColorMode } from '@vueuse/core'
 import { Icon } from '@iconify/vue'
 import { Button } from '@/components/ui/button'
-import { DropdownMenu } from '@/components/ui/dropdown-menu'
 
 const mode = useColorMode({ disableTransition: false });
 const toggleTheme = () => {
@@ -17,22 +16,20 @@ const toggleTheme = () => {
 </script>
 
 <template>
-  <DropdownMenu>
-    <Button
-      variant="outline"
-      @click="toggleTheme()"
-    >
-      <Icon
-        icon="radix-icons:moon"
-        class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
-      />
-      <Icon
-        icon="radix-icons:sun"
-        class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
-      />
-      <span
-        class="sr-only"
-      >Toggle theme</span>
-    </Button>
-  </DropdownMenu>
+  <Button
+    variant="outline"
+    @click="toggleTheme()"
+  >
+    <Icon
+      icon="radix-icons:moon"
+      class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+    />
+    <Icon
+      icon="radix-icons:sun"
+      class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+    />
+    <span
+      class="sr-only"
+    >Toggle theme</span>
+  </Button>
 </template>
